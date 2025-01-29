18a2aeecad7cb233a23a80af6ad972a233af8f1b
Farhan-Ganayim <f.ganayim@gmail.com>
2025-01-29 14:27:08 +0200

    Initial upload and animal list implementation

---
+++ proj-basics/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { UserPreview } from '../cmps/UserPreview.jsx'
+import { AnimalsList } from '../cmps/Animals.jsx'
+
+export function Home() {
+    const animalInfos = [
+        { type: 'Malayan Tiger', count: 787 },
+        { type: 'Mountain Gorilla', count: 212 },
+        { type: 'Fin Whale', count: 28 },
+    ]
+
+    return (
+        <section>
+            <h2>Home Sweet Home</h2>
+            <UserPreview />
+            <AnimalsList animalInfos={animalInfos}/>
+            
+        </section>
+    )
+}
